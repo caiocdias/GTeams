757a78f09b96d5703c19b2ab6e8657960da5820e
Caio Cezar Dias <caiocd007@gmail.com>
2025-03-12 15:41:50 -0300

    [Feature]: DateDimension Dtos

---
+++ GTeams-backend/Dtos/DateDimensionDtos/DateDimensionReturnDto.cs
@@ -0,0 +1,17 @@
+using GTeams_backend.Models;
+
+namespace GTeams_backend.Dtos.DateDimensionDtos;
+
+public class DateDimensionReturnDto
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+
+    public int TotalBusinessDays { get; set; }
+    
+    public ICollection<Team> Teams { get; set; } = new List<Team>();
+}

4db0f43a378b0a2413da0aa5b5c34ef51d2a4490
Caio Cezar Dias <caiocd007@gmail.com>
2025-03-27 11:24:05 -0300

    Lógica de negócios inical para colaborador

---
+++ GTeams-backend/Dtos/ColaboradorDtos/InserirColaboradorDto.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using GTeams_backend.Dtos.EmailDtos;
+using GTeams_backend.Dtos.MatriculaDtos;
+using GTeams_backend.Models.Enums;
+
+namespace GTeams_backend.Dtos.ColaboradorDtos;
+
+public class InserirColaboradorDto
+{
+    [StringLength(100)]
+    public string Nome { get; set; } = string.Empty;
+    
+    [Required]
+    public Funcao Funcao { get; set; }
+    
+    [StringLength(14)]
+    [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", ErrorMessage = "CPF inválido. Use o formato 000.000.000-00.")]
+    public string Cpf { get; set; } = "000.000.000-00";
+    
+    public int EquipeId { get; set; }
+    
+    public ICollection<InserirMatriculaDto> Matriculas { get; set; } = new List<InserirMatriculaDto>();
+    public ICollection<InserirEmailDto> Emails { get; set; } = new List<InserirEmailDto>();
+    
+    public string Password { get; set; } = string.Empty;
+}
---
+++ GTeams-backend/Services/ColaboradorService.cs
@@ -0,0 +1,14 @@
+using GTeams_backend.Data;
+using GTeams_backend.Dtos.ColaboradorDtos;
+using GTeams_backend.Models;
+
+namespace GTeams_backend.Services;
+
+public class ColaboradorService(AppDbContext _appDbContext)
+{
+    /*
+    public async Task<RetornarColaboradorDto> InserirColaboradorAsync(InserirColaboradorDto inserirColaboradorDto)
+    {
+        
+    } */
+}
